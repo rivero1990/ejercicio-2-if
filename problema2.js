a4cfb69a5445ac222e79bd8e8520b0afa4792a48
Anibal Rivero <riveroleonel@gmail.com>
2023-04-25 14:21:05 -0300

    modificacion ejerecio if 2

--- problema2.js
+++ problema2.js
@@ -1,15 +1,18 @@
 const CONVERSION_DOLAR = 39684.40;
 const CONVERSION_ETHEREUM = 13.5400;
+const CANT_BTC_DEF = 0;
 
 let btcIngresados = prompt("Ingrese la cantidad de bitcoins");
 let consultaCambio = prompt("Desea dolares o ethereum");
 let eleccionPrimera = "dolares";
 let eleccionSegunda = "ethereum";
 
-if (consultaCambio == eleccionPrimera) {
+if ((consultaCambio == eleccionPrimera) && (btcIngresados > CANT_BTC_DEF)) {
     document.write("Su conversion es " + (CONVERSION_DOLAR * btcIngresados) + " dolares");
-} else if (consultaCambio == eleccionSegunda) {
+} else if ((consultaCambio == eleccionSegunda) && (btcIngresados > CANT_BTC_DEF)) {
     document.write("Su conversion es " + (CONVERSION_ETHEREUM * btcIngresados) + " ethereum");
-}else {
-    document.write("No se puede realizar la conversion");
+} 
+
+else {
+    document.write("Numero de bitcoins o moneda a cambiar invalido : intente nuevamente");
 }
